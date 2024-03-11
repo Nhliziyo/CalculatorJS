81f716da080c6669cb1b35f8bd2d4550b1b33144
Nhliziyo Sibiya <nhliziyosibiya@gmai.com>
2024-03-11 23:36:06 +0200

    Day 2

---
+++ pageControl.js
@@ -0,0 +1,6 @@
+//use count var for brackets odd "nums = ( " and "even nums = )"
+function output(txt){
+    let text = element.innerText;
+    document.querySelector(".display").value += txt;
+    //if (element.class === "Number")
+}
