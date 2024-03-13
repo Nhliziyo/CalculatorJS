48361c23b75a0e06111a3b78366a1084710ab8a4
Nhliziyo Sibiya <nhliziyosibiya@gmai.com>
2024-03-13 21:06:06 +0200

    Day 4

--- pageControl.js
+++ pageControl.js
@@ -1,6 +1,36 @@
 //use count var for brackets odd "nums = ( " and "even nums = )"
-function output(){
-    const text = element.textContent;
+let count = 0;
+function output(button){
+    let text = button.textContent;    
     document.querySelector(".display").innerHTML += text;
-    //if (element.class === "Number")
+}
+
+function brackets() {
+    if (count%2 === 0) {
+        document.querySelector(".display").innerHTML += "(";
+        count++;
+    }
+    else {
+        document.querySelector(".display").innerHTML += ")";
+        count++;
+    }
+}
+
+function equalTo() {
+    let expression = document.querySelector(".display").innerHTML;
+    if (count%2 !== 0) {
+        alert("you're missing a bracket");
+        return;
+    }
+    try{
+        const answer = eval(expression);
+        document.querySelector(".display").innerHTML = answer;
+    }
+    catch {
+        alert("you're doing something wrong, try again");
+    }
+}
+
+function suitableForm(expression) {
+    return expression.replace()
 }
